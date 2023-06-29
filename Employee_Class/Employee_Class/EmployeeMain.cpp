7e800daa0f6a00463d446be0825ddb92e95cdb93
samanthagonzales14 <sgg8xd@umsystem.edu>
2023-06-29 13:36:32 -0500

    Assignment2

--- Employee_Class/Employee_Class/EmployeeMain.cpp
+++ Employee_Class/Employee_Class/EmployeeMain.cpp
@@ -1,12 +1,19 @@
 #include "Employee.h"
 #include "Professional.h"
 #include "Nonprofessional.h"
+#include <vector>
 
 int main() {
-	Professional pro1("Diana", 35, 40, 0.12); // professional instance
-	Nonprofessional nonPro1("Alyssa", 25, 24, 0.10); // nonprofessional instance
+	vector<Employee*> myEmployees;		// store employees into vector of employee type
+	Professional* pro1 = new Professional("Diana", 35, 40, 0.12); // professional instance
+	Nonprofessional* nonPro1 = new Nonprofessional("Alyssa", 25, 24, 0.10); // nonprofessional instance
 
-	pro1.print();
-	nonPro1.print();
+	myEmployees.push_back(pro1);	// add employee to vector
+	myEmployees.push_back(nonPro1); // add employee to vector
 
+	for (int i = 0; i < myEmployees.size(); i++) {
+		myEmployees.at(i)->print();		// print each employee in vector
+	}
+	
+	return 0;
 }
